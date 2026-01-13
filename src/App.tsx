b1d7df9cd251e1bbb8e7230e0dbb33bb910d5a2e
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-13 15:50:57 +0700

    Building a Wrapper Component That Renders Different Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Input from "./components/Input"
+import Button from "./components/Button"
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      {/* <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" /> */}
+
+      <p>
+        <Button el="button">Test Button</Button>
+      </p>
+
+      <p>
+        <Button el="anchor" href="https://www.vlu.edu.vn/" target="_blank">
+          Link
+        </Button>
+      </p>
     </main>
   )
 }
