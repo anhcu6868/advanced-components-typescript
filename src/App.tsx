b1e8368d9dd664e4f4b2d3e5b85ce0d1a1fca2d0
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-15 23:04:21 +0700

    Exposing Component APIs with useImperativeHandle (with TypeScript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Input from "./components/Input"
 import Form from "./components/Form"
 import Button from "./components/Button"
+import { useRef } from "react"
+import { type FormHandle } from "./components/Form"
 
 function App() {
+  const customForm = useRef<FormHandle>(null)
   const handleSave = (data: unknown) => {
     const extractedData = data as { name: string; age: string }
     console.log(extractedData)
+    customForm.current?.clear()
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,43 @@
-import type { ComponentPropsWithoutRef, FormEvent } from "react"
+import {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  type ComponentPropsWithoutRef,
+  type FormEvent,
+} from "react"
+
+export type FormHandle = {
+  clear: () => void
+}
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void
 }
 
-const Form = ({ onSave, children, ...otherProps }: FormProps) => {
-  // Handle Submit Form
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const data = Object.fromEntries(formData)
-    onSave(data)
+const Form = forwardRef<FormHandle, FormProps>(
+  ({ onSave, children, ...otherProps }, ref) => {
+    const form = useRef<HTMLFormElement>(null)
+    useImperativeHandle(ref, () => {
+      return {
+        clear() {
+          console.log("Clearing the form")
+          form.current?.reset()
+        },
+      }
+    })
+    // Handle Submit Form
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      const formData = new FormData(event.currentTarget)
+      const data = Object.fromEntries(formData)
+      onSave(data)
+    }
+    return (
+      <form onSubmit={handleSubmit} {...otherProps} ref={form}>
+        {children}
+      </form>
+    )
   }
-  return (
-    <form onSubmit={handleSubmit} {...otherProps}>
-      {children}
-    </form>
-  )
-}
+)
 
 export default Form
